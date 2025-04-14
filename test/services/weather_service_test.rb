bca289617b8d4bd7a83ad3258365686c2583b27e
Christopher Wilde <chris@chriswilde.dev>
2025-04-13 21:07:25 -0400

    Update test to new service

--- test/services/weather_service_test.rb
+++ test/services/weather_service_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class WeatherServiceTest < ActiveSupport::TestCase
   test "call with invalid lat/long" do
     assert_raises IOError do
-      WeatherService.retrieve(190, 190)
+      WeatherService.new(190, 190)
     end
   end
 
@@ -12,12 +12,10 @@ class WeatherServiceTest < ActiveSupport::TestCase
     latitude = 37.331669
     longitude = -122.030098
 
-    weather = WeatherService.retrieve(latitude, longitude)
-    assert_includes 0..100, weather.temperature
-    assert_includes 0..100, weather.temperature_min
-    assert_includes 0..100, weather.temperature_max
-    assert_includes 0..100, weather.humidity
-    assert_includes 900..1100, weather.pressure
-    refute_empty weather.description
+    weather = WeatherService.new(latitude, longitude)
+    assert_includes 0..100, weather.current.temperature
+    assert_includes 0..100, weather.current.humidity
+    assert_includes 900..1100, weather.current.pressure
+    refute_empty weather.current.description
   end
 end
